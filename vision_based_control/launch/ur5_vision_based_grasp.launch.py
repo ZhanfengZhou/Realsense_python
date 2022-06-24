ffe7294736dab684809225d32b08825f12c05c5b
ZhanfengZhou <zhanfeng.zhou@mail.utoronto.ca>
2022-06-24 17:48:00 -0400

    vision-based grasp finished

--- vision_based_control/launch/ur5_vision_based_grasp.launch.py
+++ vision_based_control/launch/ur5_vision_based_grasp.launch.py
@@ -6,8 +6,9 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
-    online_camera_calibration_goals = PathJoinSubstitution(
-        [FindPackageShare("vision_based_control"), "config", "ur5_3d_construction_move.yaml"]
+
+    vision_based_grasp_objects_goals = PathJoinSubstitution(
+        [FindPackageShare("vision_based_control"), "config", "ur5_vision_based_grasp.yaml"]
     )
 
 
@@ -15,9 +16,9 @@ def generate_launch_description():
         [
             Node(
                 package="ros2_control_test_nodes",
-                executable="publisher_camera_calibration_ik_controller",
-                name="publisher_ur5_3d_construction_move_controller",
-                parameters=[online_camera_calibration_goals],
+                executable="publisher_vision_based_grasp_controller",
+                name="publisher_vision_based_grasp_controller",
+                parameters=[vision_based_grasp_objects_goals],
                 output={
                     "stdout": "screen",
                     "stderr": "screen",
